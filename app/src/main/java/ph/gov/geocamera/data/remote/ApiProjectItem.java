f4f0986b16542bd2454b308ba2d98be10198eefa
Marvin Palapuz <marvinpalapuz25@gmail.com>
2026-09-09 01:35:42 +0800

    Add capture target metadata to Android project model

--- app/src/main/java/ph/gov/geocamera/data/remote/ApiProjectItem.java
+++ app/src/main/java/ph/gov/geocamera/data/remote/ApiProjectItem.java
@@ -7,4 +7,14 @@ public class ApiProjectItem {
     public String beneficiary;
     public String location;
     public double cost;
+
+    // Unified /capture-targets metadata.
+    public String projectType;
+    public String divisionId;
+    public String divisionCode;
+    public String divisionName;
+    public String projectImplementors;
+    public String projectDescription;
+    public String dateFrom;
+    public String dateTo;
 }
