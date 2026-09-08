a282bb3a50c974b1e6cdda87832739366cc9b615
Marvin Palapuz <marvinpalapuz25@gmail.com>
2026-09-08 13:22:29 +0800

    Move project sync to migrated GeoKlik API

--- app/src/main/java/ph/gov/geocamera/data/remote/ProjectApiService.java
+++ app/src/main/java/ph/gov/geocamera/data/remote/ProjectApiService.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class ProjectApiService {
 
     private static final String TAG = "PROJECT_API";
-    private static final String PROJECTS_URL = "https://app.philmech.gov.ph/geomap_api/api/projects";
+    private static final String PROJECTS_URL = ApiEndpoints.PROJECTS_URL;
 
     public List<ApiProjectItem> fetchProjects() throws Exception {
         HttpURLConnection conn = null;
